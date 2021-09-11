a8af09d48bf145d285f5a61369ecf63c4506c95f
Roger Ferro <rogerferro@gmail.com>
2021-09-11 18:29:09 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvendas4/Dsvendas4Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendas4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsvendas4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsvendas4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvendas4/Dsvendas4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendas4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsvendas4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
